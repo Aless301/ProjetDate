82e068dd00293891632dde6b0a7d44fbff0851ac
chitaltug <chitaltug@gmail.com>
2024-01-26 09:03:09 +0100

    Create reponse.php
    Tableaux des reponses questions.

---
+++ reponse.php
@@ -0,0 +1,24 @@
+<?php
+$q1 = array (
+[1,'Holà misstinguette, comment tu vas ?']
+[2,'Salut ça va ?']
+[3,'Excuse moi tu aurais pas fais la guerre ? car tu es canon !']
+)
+
+$q2 = array (
+[1,'']
+[2,'']
+[3,'']
+)
+
+$q3 = array (
+[1,'']
+[2,'']
+[3,'']
+)
+
+$q4 = array (
+[1,'']
+[2,'']
+[3,'']
+)
