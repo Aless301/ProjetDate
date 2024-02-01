2dc8a63239874ef0097a7d1dafaeb66cd09ead05
Ora301 <149436401+Ora301@users.noreply.github.com>
2024-02-01 09:00:56 +0100

    1.2
    Le système de question est en cours de création.
    Il est terminer à 50%

--- index.php
+++ index.php
@@ -6,21 +6,26 @@ include('includes/header.php');
 </div>
 
 <fieldset>
-<legend>Quand est mort John Fitzgerald Kennedy, le 35e président des Etats-Unis?</legend>
-<form action="quiz2.php" method="post">
-<input type="radio" id="president" name="president" value="22 novembre 1963" required>
-<label for="president">Le 22 novembre 1963</label><br>
+<legend>Comment Lancer la discussion ? </legend>
+<form action="index.php" method="post">
+<input type="radio" id="r1" name="r1" value="1" required>
+<label for="president">Holà misstinguette, comment tu vas ?</label><br>
 
-    <input type="radio" id="president1" name="president" value="22 janvier 1963">
-<label for="president1">Le 22 janvier 1963</label><br>
+    <input type="radio" id="r2" name="r2" value="2">
+<label for="president1">Salut ça va ?</label><br>
 
-    <input type="radio" id="president2" name="president" value="22 Juin 1963">
-<label for="president2">Le 22 Juin 1963</label><br>
+    <input type="radio" id="r3" name="r3" value="3">
+<label for="president2">Excuse moi tu aurais pas fais la guerre ? Car tu es canon !</label><br>
 
     <input class="bouton" type="submit" value="↪">
 
   </form>
 </fieldset>
+<?php
+if(isset($_POST['r1'])){
+    echo'Salut monsieur, jolie photo.';
+}
+?>
 
 <?php
 include('includes/footer.php');
