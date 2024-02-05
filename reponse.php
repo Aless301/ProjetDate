a47c8bff9aa922a49baa9f31c94e37d02037ca35
chitaltug <chitaltug@gmail.com>
2024-02-05 09:33:35 +0100

    Réponses questions
    Réponses des 7 premières questions avec V, N , F.

--- reponse.php
+++ reponse.php
@@ -6,39 +6,39 @@ $q1 = array (
 )
 
 $q2 = array (
-[1,'']
-[2,'']
-[3,'']
+[1,'Si tu me le permets.']
+[2,'Oui.']
+[3,'Non.']
 )
 
 $q3 = array (
-[1,'']
-[2,'']
-[3,'']
+[1,'Oui , je suis très proche et au petit soin']
+[2,'Oui et non car on se voit pas trop']
+[3,"Non j'ai fais une jeffrey dahmer"]
 )
 
 $q4 = array (
-[1,'']
-[2,'']
-[3,'']
+[1,"Oui, j'aime le classique"]
+[2,"J'aime les musiques de soirées"]
+[3,"J'aime le rock country et le rap indien"]
 )
 
 $q5 = array (
-[1,'']
-[2,'']
-[3,'']
+[1,"Oui, j'ai fais top chef junior en Franche-comté"]
+[2,"J'ai déjà fais des pâtes"]
+[3,"La place de la femme c'est à la KUISINE"]
 )
 
 $q6 = array (
-[1,'']
-[2,'']
-[3,'']
+[1,'1 verre']
+[2,'2 verres']
+[3,'3 verres']
 )
 
 $q7 = array (
-[1,'']
-[2,'']
-[3,'']
+[1,'Action : faire un slow avec la musique seulement']
+[2,'Action :chifoumi']
+[3,"Action : regarder un film, si tu t'endors sur le côté cu canapé et elle se vexe"]
 )
 
 $q8 = array (
