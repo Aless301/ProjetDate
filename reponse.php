d6cc9f55fb4ab4fc0f9d3bede19d211acc237918
chitaltug <chitaltug@gmail.com>
2024-02-15 09:00:08 +0100

    Update reponse.php
    réorganisation des questions + suite des questions

--- reponse.php
+++ reponse.php
@@ -12,39 +12,39 @@ $q2 = array (
 )
 
 $q3 = array (
-[1,'Oui , je suis très proche et au petit soin']
-[2,'Oui et non car on se voit pas trop']
-[3,"Non j'ai fais une jeffrey dahmer"]
+[1,'Oui , je suis très proche et au petit soin.']
+[2,'Oui et non car on se voit pas trop.']
+[3,"Non j'ai fais une jeffrey dahmer."]
 )
 
 $q4 = array (
-[1,"Oui, j'aime le classique"]
-[2,"J'aime les musiques de soirées"]
-[3,"J'aime le rock country et le rap indien"]
+[1,"Oui, j'aime le classique."]
+[2,"J'aime les musiques de soirées."]
+[3,"J'aime le rock country et le rap indien."]
 )
 
 $q5 = array (
-[1,"Je m'habille en old money"]
-[2,"Je suis fashion-week du streetwear"]
-[3,"Je m'habille gothique"]
+[1,"Je m'habille en old money."]
+[2,"Je suis fashion-week du streetwear."]
+[3,"Je m'habille gothique."]
 )
 
 $q6 = array (
-[1,"Oui, j'ai fais top chef junior en Franche-comté"]
-[2,"J'ai déjà fais des pâtes"]
-[3,"La place de la femme c'est à la KUISINE"]
+[1,"Oui, j'ai fais top chef junior en Franche-comté."]
+[2,"J'ai déjà fais des pâtes."]
+[3,"La place de la femme c'est à la KUISINE."]
 )
 
 $q7 = array (
-[1,'1 verre']
-[2,'2 verres']
-[3,'3 verres']
+[1,'1 verre.']
+[2,'2 verres.']
+[3,'3 verres.']
 )
 
 $q8 = array (
-[1,'Action : faire un slow avec la musique seulement']
-[2,'Action :chifoumi']
-[3,"Action : regarder un film, si tu t'endors sur le côté cu canapé et elle se vexe"]
+[1,'Action : faire un slow avec la musique seulement.']
+[2,'Action :chifoumi.']
+[3,"Action : regarder un film, si tu t'endors sur le côté cu canapé et elle se vexe."]
 )
 
 $q9 = array (
@@ -54,13 +54,25 @@ $q9 = array (
 )
 
 $q10 = array (
-[1,""]
-[2,""]
-[3,""]
+[1,"Action : Tout petit bisou sur la joue."]
+[2,"Action : Lui dire que c'était chiant."]
+[3,"Action : La pousser de la colline."]
 )
 
 $q11 = array (
-[1,'']
-[2,'']
-[3,'']
+[1,"Regarder un live twitch sur le grand écran ?"]
+[2,"Aller regarder la lune au télescope."]
+[3,"Parler de voiture."]
+)
+
+$q12 = array (
+[1,"Talmo, je suis un grand fan de nuuueil."]
+[2,"Petit squeezie, il live aujourd'hui."]
+[3,"Sardoche, j'adore league of legends et ne pas me laver."]
+)
+
+$q13 = array (
+[1,"."]
+[2,"."]
+[3,"."]
 )
