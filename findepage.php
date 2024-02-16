82eb26e5f13af812ee95461c72111843d73e0cea
Tim-055 <156292798+Tim-055@users.noreply.github.com>
2024-02-16 09:00:00 +0100

    Create findepage.php
    page de fin

---
+++ findepage.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projet Date</title>
+    <header>
+
+    <php
+    
+
+                if ($_SESSION["score"] > 100) {
+                    echo "CHAMPIOOON". $SCORE_SESSION["score"] ;
+                    <img src="https://www.gif-maniac.com/gifs/53/53118.gif" alt="Trop Cooool!">
+
+                    
+                } elseif ($_SESSION["score"] < 100 && $_SESSION["score"] ==0 ) {
+                    echo "This is the friendzone";
+                    <img src="https://usagif.com/wp-content/uploads/gif/crying-96.gif" alt="Triste">
+
+
+                } else {
+                    echo "Elle vous a bloquer et a posé plainte contre vous la police, votre famille vous rejette, vous avez déshérité et vous finissez en prison.";
+                <img src="https://media1.tenor.com/m/72NJfF7QTl4AAAAC/working-get-to-work.gif" alt="Prison">}
+?>
+</header>
+
+
+
+
+<footer>
+
+</footer>
