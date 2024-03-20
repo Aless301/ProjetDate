59e896bd30ac284469ca135e3e91ea397d89b092
GareAuPoireau <106329877+GareAuPoireau@users.noreply.github.com>
2024-03-20 01:27:01 +0100

    pleins de modif et je suis fatigué jspr j'ai pas tout cassé

--- personnage/custompersonnage.php
+++ personnage/custompersonnage.php
@@ -3,28 +3,65 @@ include('../includes/header.php');
 ?>
 
 <script>
-        function afficherImage(image) {
-            document.getElementById("image_affichee").src = image;
-        }
-        afficherImage('../images/tete1.jpg');
-    </script>
-    <h2>Choisissez une image :</h2>
-    <form method="post" action="resultat_personnage.php">
-        <label for="image">Choisissez une image :</label><br><br>
+    function afficherTete(image) {
+        document.getElementById("tete_affichee").src = image;
+    }
+    function afficherCorps(image) {
+        document.getElementById("corps_affichee").src = image;
+    }
+    function afficherJambes(image) {
+        document.getElementById("jambes_affichee").src = image;
+    }
+    afficherTete('../images/tete1.jpg');
+    afficherCorps('../images/corps1.jpg');
+    afficherJambes('../images/jambes1.jpg');
+</script>
 
-        <input type="radio" id="image1" name="image" value="../images/tete1.jpg" onclick="afficherImage('../images/tete1.jpg')">
-        <label for="image1"><img src="../images/tete1.jpg" alt="Tête 1"></label><br>
+<h2>Créez votre personnage :</h2>
+<form method="post" action="resultat_personnage.php">
+    <div>
+        <h3>Choisissez une tête :</h3>
+        <input type="radio" id="tete1" name="tete" value="tete1" onclick="afficherTete('../images/tete1.jpg')" checked>
+        <label for="tete1"><img src="../images/tete1.jpg" alt="Tête 1"></label>
 
-        <input type="radio" id="image2" name="image" value="../images/tete2.jpg" onclick="afficherImage('../images/tete2.jpg')">
-        <label for="image2"><img src="../images/tete2.jpg" alt="Tête 2"></label><br>
+        <input type="radio" id="tete2" name="tete" value="tete2" onclick="afficherTete('../images/tete2.jpg')">
+        <label for="tete2"><img src="../images/tete2.jpg" alt="Tête 2"></label>
 
-        <input type="radio" id="image3" name="image" value="../images/tete3.jpg" onclick="afficherImage('../images/tete3.jpg')">
-        <label for="image3"><img src="../images/tete3.jpg" alt="Tête 3"></label><br><br>
+        <input type="radio" id="tete3" name="tete" value="tete3" onclick="afficherTete('../images/tete3.jpg')">
+        <label for="tete3"><img src="../images/tete3.jpg" alt="Tête 3"></label>
+    </div>
 
-        <img id="image_affichee" src="" alt="Aucune image sélectionnée"><br><br>
+    <div>
+        <h3>Choisissez un corps :</h3>
+        <input type="radio" id="corps1" name="corps" value="corps1" onclick="afficherCorps('../images/corps1.jpg')" checked>
+        <label for="corps1"><img src="../images/corps1.jpg" alt="Corps 1"></label>
 
-        <input type="submit" name="submit" value="Valider">
-    </form>
+        <input type="radio" id="corps2" name="corps" value="corps2" onclick="afficherCorps('../images/corps2.jpg')">
+        <label for="corps2"><img src="../images/corps2.jpg" alt="Corps 2"></label>
+
+        <input type="radio" id="corps3" name="corps" value="corps3" onclick="afficherCorps('../images/corps3.jpg')">
+        <label for="corps3"><img src="../images/corps3.jpg" alt="Corps 3"></label>
+    </div>
+
+    <div>
+        <h3>Choisissez des jambes :</h3>
+        <input type="radio" id="jambes1" name="jambes" value="jambes1" onclick="afficherJambes('../images/jambes1.jpg')" checked>
+        <label for="jambes1"><img src="../images/jambes1.jpg" alt="Jambes 1"></label>
+
+        <input type="radio" id="jambes2" name="jambes" value="jambes2" onclick="afficherJambes('../images/jambes2.jpg')">
+        <label for="jambes2"><img src="../images/jambes2.jpg" alt="Jambes 2"></label>
+
+        <input type="radio" id="jambes3" name="jambes" value="jambes3" onclick="afficherJambes('../images/jambes3.jpg')">
+        <label for="jambes3"><img src="../images/jambes3.jpg" alt="Jambes 3"></label>
+    </div>
+
+    <h3>Aperçu :</h3>
+    <img id="tete_affichee" src="" alt="Aperçu de la tête"><br>
+    <img id="corps_affichee" src="" alt="Aperçu du corps"><br>
+    <img id="jambes_affichee" src="" alt="Aperçu des jambes"><br><br>
+
+    <input type="submit" name="submit" value="Valider la création">
+</form>
 
 <?php
 include('../includes/footer.php');
--- personnage/resultat_personnage.php
+++ personnage/resultat_personnage.php
@@ -6,21 +6,37 @@ include('../includes/header.php');
     <h2>Résultat de la création du personnage</h2>
     <?php
     if(isset($_POST['submit'])){
-        if(isset($_POST['image'])){
-            $image = $_POST['image'];
-            echo "<img src='$image' alt='Image choisie'>";
-        } else {
-            echo "Aucune image sélectionnée.";
+
+        if(isset($_POST['tete'])){
+            $tete = $_POST['tete'];
+            echo "<h3>Tête choisie :</h3>";
+            echo "<img src='../images/{$tete}.jpg' alt='Tête choisie' style='width:100px;'><br>";
         }
+
+    
+        if(isset($_POST['corps'])){
+            $corps = $_POST['corps'];
+            echo "<h3>Corps choisi :</h3>";
+            echo "<img src='../images/{$corps}.jpg' alt='Corps choisi' style='width:100px;'><br>";
+        }
+
+     
+        if(isset($_POST['jambes'])){
+            $jambes = $_POST['jambes'];
+            echo "<h3>Jambes choisies :</h3>";
+            echo "<img src='../images/{$jambes}.jpg' alt='Jambes choisies' style='width:100px;'><br>";
+        }
+    } else {
+        echo "Aucune sélection n'a été faite.";
     }
     ?>
 
 <form method="post" action="custompersonnage.php">
-    <input type="submit" name="perso" value="moche">
-</form>
-<form method="post" action="../q1.php">
-    <input type="submit" name="jeu" value="vers le jeu">
-</form>
+        <input type="submit" name="perso" value="Modifier le personnage">
+    </form>
+    <form method="post" action="../q1.php">
+        <input type="submit" name="jeu" value="Vers le jeu">
+    </form>
     <?php
     include('../includes/footer.php');
     ?>
