b5fee2ebd1bc325e6780c7a0f6b9d9cf7ab2e9aa
Ora301 <149436401+Ora301@users.noreply.github.com>
2024-03-25 08:35:35 +0100

    Réparation Pseudo

--- login.php
+++ login.php
@@ -7,18 +7,18 @@ include('includes/header.php');
 </div>
 <div class="divLogin">
 <form class="bouton" method="post" action="login.php" >
-    <input type="text" name="pseudo" placeholder="exemple : poireau">
+    <input type="text" name="pseado" placeholder="exemple : poireau">
     <input type="submit" name="valider" value="Valider ! ">
 </form>
 <?php
-if (isset($_POST['pseudo'])){
-    $_SESSION['pseado']="";
+if (isset($_POST['pseado'])){
+    $_SESSION['pseudo']="";
 }
-if (isset($_POST['pseudo'])){
-    $_SESSION['pseudo']=$_POST['pseudo'];
+if (isset($_POST['pseado'])){
+    $_SESSION['pseudo']=$_POST['pseado'];
 }
-if (isset($_POST['pseudo'])){
-echo '<p>Es tu sûr de vouloir "'.$_SESSION['pseudo'].'" Comme Pseudo ? </br> Si oui la suite est ici : <a href="q1.php">Le jeu !</a></p>';
+if (isset($_POST['pseado'])){
+echo '<p>Es tu sûr de vouloir "'.$_SESSION['pseudo'].'" Comme Pseudo ? </br> Si oui la suite est ici : <a href="personnage/custompersonnage.php">Le jeu !</a></p>';
 }
 ?>
 </div>
