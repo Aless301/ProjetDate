b7c41f891aecb3c9889f5f63b60155b43bfa5238
1lyasss <ilyass.ajyache@eleve.cndbs.be>
2024-03-25 08:14:39 +0100

    changement de la page php de login

--- login.php
+++ login.php
@@ -1,25 +1,30 @@
 <?php
 include('includes/header.php');
 ?>
-<div style="text-align: center;">
-    <div class="divLogin">
-        <form class="bouton" method="post" action="login.php">
-            <input type="text" name="pseudo" placeholder="Votre pseudo...">
-            <input type="submit" name="valider" value="Valider ! ">
-        </form>
-        <?php
-        if (isset($_POST['pseudo'])) {
-            $_SESSION['pseado'] = "";
-        }
-        if (isset($_POST['pseudo'])) {
-            $_SESSION['pseudo'] = $_POST['pseudo'];
-        }
-        if (isset($_POST['pseudo'])) {
-            echo '<p>Es tu sûr de vouloir "' . $_SESSION['pseudo'] . '" Comme Pseudo ? </br> Si oui la suite est ici : </br> <a href="personnage/custompersonnage.php">Le jeu !</a></p>';
-        }
-        ?>
-    </div>
+
+<div class="txtwelcome">
+    <p>Bienvenue dans le ProjetDate</br>Veuillez indiquer votre pseudo ci-dessous</p>
 </div>
+<div class="divLogin">
+<form class="bouton" method="post" action="login.php" >
+    <input type="text" name="pseudo" placeholder="exemple : poireau">
+    <input type="submit" name="valider" value="Valider ! ">
+</form>
+<?php
+if (isset($_POST['pseudo'])){
+    $_SESSION['pseado']="";
+}
+if (isset($_POST['pseudo'])){
+    $_SESSION['pseudo']=$_POST['pseudo'];
+}
+if (isset($_POST['pseudo'])){
+echo '<p>Es tu sûr de vouloir "'.$_SESSION['pseudo'].'" Comme Pseudo ? </br> Si oui la suite est ici : <a href="q1.php">Le jeu !</a></p>';
+}
+?>
+</div>
+
+
+
 <?php
 include('includes/footer.php');
 ?>
