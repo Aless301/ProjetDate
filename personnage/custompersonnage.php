7e5e860bc2414d5d81d787235e4a9d8c97ae2e5a
1lyasss <ilyass.ajyache@eleve.cndbs.be>
2024-03-31 19:26:22 +0200

    Question Carte ID
    Je n'arrive pas à faire les conditions sur la tete

--- personnage/custompersonnage.php
+++ personnage/custompersonnage.php
@@ -1,5 +1,19 @@
 <?php
 include('../includes/header.php');
+
+// Vérifier si une valeur de tête a été soumise via le formulaire POST
+if(isset($_POST['tete'])){
+    $_SESSION['tete'] = $_POST['tete']; // Stocker la valeur de tête dans la session
+}
+
+// Initialiser la valeur de tête
+$tete = isset($_SESSION['tete']) ? $_SESSION['tete'] : null;
+
+// Vérifier si une valeur de tête est définie dans la session
+if(isset($_SESSION['tete'])){
+    $tete = $_SESSION['tete'];
+}
+
 ?>
 
 <script>
@@ -9,22 +23,19 @@ include('../includes/header.php');
     function afficherCorps(image) {
         document.getElementById("corps_affichee").src = image;
     }
-  
-    afficherTete('../images/tete1.jpg');
-    afficherCorps('../images/corps1.jpg');
 </script>
 
 <h2>Créez votre personnage :</h2>
 <form method="post" action="resultat_personnage.php">
     <div>
         <h3>Choisissez une tête :</h3>
-        <input type="radio" id="tete1" name="tete" value="tete1" onclick="afficherTete('../images/tete1.jpg')" checked>
+        <input type="radio" id="tete1" name="tete" value="tete1" onclick="afficherTete('../images/tete1.jpg')" <?php if ($tete == 'tete1') echo 'checked'; ?>>
         <label for="tete1"><img src="../images/tete1.jpg" alt="Tête 1"></label>
 
-        <input type="radio" id="tete2" name="tete" value="tete2" onclick="afficherTete('../images/tete2.jpg')">
+        <input type="radio" id="tete2" name="tete" value="tete2" onclick="afficherTete('../images/tete2.jpg')" <?php if ($tete == 'tete2') echo 'checked'; ?>>
         <label for="tete2"><img src="../images/tete2.jpg" alt="Tête 2"></label>
 
-        <input type="radio" id="tete3" name="tete" value="tete3" onclick="afficherTete('../images/tete3.jpg')">
+        <input type="radio" id="tete3" name="tete" value="tete3" onclick="afficherTete('../images/tete3.jpg')" <?php if ($tete == 'tete3') echo 'checked'; ?>>
         <label for="tete3"><img src="../images/tete3.jpg" alt="Tête 3"></label>
     </div>
 
@@ -41,8 +52,8 @@ include('../includes/header.php');
     </div>
 
     <h3>Aperçu :</h3>
-    <img id="tete_affichee" src="" alt="Aperçu de la tête"><br>
-    <img id="corps_affichee" src="" alt="Aperçu du corps"><br>
+    <img id="tete_affichee" src="../images/<?php echo $tete; ?>.jpg" alt="Aperçu de la tête" style="width:100px;"><br>
+    <img id="corps_affichee" src="../images/corps1.jpg" alt="Aperçu du corps" style="width:100px;"><br>
     
     <input type="submit" name="submit" value="Valider la création">
 </form>
