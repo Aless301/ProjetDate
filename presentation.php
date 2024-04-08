2cdbb73efc84704487b382f4dbc05056c306b5d9
Ora301 <149436401+Ora301@users.noreply.github.com>
2024-04-08 09:40:58 +0200

    Début Présentation

---
+++ presentation.php
@@ -0,0 +1,11 @@
+<?php
+include('includes/header.php');
+?>
+<h1>Le but du jeu :</h1>
+<p>Le but est assez simple, le jeu "Projet Date" conciste à draguer une personne. </br> Vous devez repondre à des questions, certaines vous rapportent des points, d'autre vous en font perde. </br> Il y a aussi des questions Green Flag, qui vous font gagner plus de points. Et les questions Red Flag qui vous font perdre la partie directement.</p>
+
+
+
+<?php
+include('includes/footer.php')
+?>
