874ea4ad54dc895be84dd82f7585752267f74706
Ora301 <149436401+Ora301@users.noreply.github.com>
2024-04-20 10:12:08 +0200

    suppression acces perso pour JCO

--- login.php
+++ login.php
@@ -18,7 +18,7 @@ include('includes/header2.php'); ?>
         $_SESSION['pseudo'] = $_POST['pseado'];
     }
     if (isset($_POST['pseado'])) {
-        echo '<p>Es tu sûr de vouloir "' . $_SESSION['pseudo'] . '" Comme Pseudo ? </br> Si oui la suite est ici : <a href="' . $root . 'personnage/custompersonnage.php">Le jeu !</a></p>';
+        echo '<p>Es tu sûr de vouloir "' . $_SESSION['pseudo'] . '" Comme Pseudo ? </br> Si oui la suite est ici : <a href="' . $root . 'q1.php">Le jeu !</a></p>';
     }
     ?>
 </div>
