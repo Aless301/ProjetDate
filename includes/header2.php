7a3339b04e86489effdc17aca421eb32880ffee4
Ora301 <149436401+Ora301@users.noreply.github.com>
2024-04-20 11:09:01 +0200

    Index -> SessionCrash

--- includes/header2.php
+++ includes/header2.php
@@ -23,7 +23,6 @@
                 echo '<button class="btn"><span class="icon"><svg viewBox="0 0 175 80" width="40" height="40"><rect width="80" height="15" fill="#f0f0f0" rx="10"></rect><rect y="30" width="80" height="15" fill="#f0f0f0" rx="10"></rect><rect y="60" width="80" height="15" fill="#f0f0f0" rx="10"></rect></svg></span><span class="text" onclick="toggleDiv()">INFO</span></button> </br>';
             }
             ?>
-            <li><a href="<?= $root ?>index.php">Index</a></li>
-            <li> <a href="<?= $root ?>session_crash.php">.</a></li>
+            <li> <a href="<?= $root ?>session_crash.php">Index</a></li>
         </ul>
     </nav>
